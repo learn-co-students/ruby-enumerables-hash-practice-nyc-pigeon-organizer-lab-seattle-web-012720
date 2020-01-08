d127c8cebdb44db4dbec005482e76dfb039c04bb
Kailana Kahawaii <kailanakahawaii@gmail.com>
2020-01-08 06:31:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pp"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  pigeon_list = {} 
+  data.each do |key, value| 
+    value.each do |description, names|
+        color_gender_lives = description.to_s
+        names.each do |pigeon|
+          if pigeon_list[pigeon]
+            if pigeon_list[pigeon][key]
+              pigeon_list[pigeon][key] << color_gender_lives
+            else 
+              pigeon_list[pigeon][key] = [color_gender_lives]
+            end 
+          else 
+            pigeon_list[pigeon] = {key => [color_gender_lives]}
+          end 
+      end 
+    end 
+  end 
+  pigeon_list
+end 
+
