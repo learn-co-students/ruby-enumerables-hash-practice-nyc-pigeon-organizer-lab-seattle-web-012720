46ed388de707788cd734e1212cf649571876ff56
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-13 01:35:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |color_gen_loc, data_hash|
+    data_hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name].has_key?(color_gen_loc)
+          new_hash[name][color_gen_loc] = []
+        end
+        if !new_hash[name][color_gen_loc].include?(attribute)
+          new_hash[name][color_gen_loc] << attribute.to_s
+        end
+        
+      end
+    end
+  end
+  new_hash
 end
