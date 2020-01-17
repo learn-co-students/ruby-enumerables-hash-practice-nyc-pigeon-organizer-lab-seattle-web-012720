f716e855ecb047c27990d2f5e5252bfd0fcb2141
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-17 05:40:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+ 
+  pigeons = {}
+  data.reduce({}) do |memo, (key, value)|
+    array = value.reduce({}) do |memo2, (key2,value2)|
+      value2.each{|n| 
+        if(pigeons[n])
+          if(pigeons[n][key].include?(key2) != true)
+            pigeons[n][key.to_sym].push(key2.to_s)
+          end
+        else
+          pigeons[n] = {:color => [], :gender => [], :lives => []}
+          pigeons[n][key].push(key2.to_s)
+        end
+      }
+    end
+  end
+  pigeons
 end
