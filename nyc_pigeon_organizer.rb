01b0d3cc24338eefbef360b22aeb2d5c2634e4e0
Jack Cole <123jack.cole@gmail.com>
2020-01-20 00:22:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  
+  data.each do |attribute, data|
+    if attribute == :color 
+      data.each do |color, names|
+        names.each do |index|
+          if new_pigeon_hash[index]
+            new_pigeon_hash[index][:color] << color.to_s 
+          else
+            new_pigeon_hash[index] = {:color => [color.to_s]}
+          end
+        end
+      end
+    end
+    if attribute == :gender
+      data.each do |gender, names|
+        names.each do |index|
+          if new_pigeon_hash[index].include?(:gender)
+            new_pigeon_hash[index][:gender] << gender.to_s 
+          else
+            new_pigeon_hash[index][:gender] = [gender.to_s]
+          end
+        end
+      end
+    end
+    if attribute == :lives
+      data.each do |location, names|
+        names.each do |index|
+          if new_pigeon_hash[index].include?(:lives)
+            new_pigeon_hash[index][:lives] << location
+          else
+            new_pigeon_hash[index][:lives] = [location]
+          end
+        end
+      end
+    end
+  end
+  p new_pigeon_hash
 end
