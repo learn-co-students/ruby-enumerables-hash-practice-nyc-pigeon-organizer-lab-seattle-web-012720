bf977d36b99ea0656b668deeb03fc73e992e42ef
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-21 02:40:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_pigeon_list = {}
+#Gather Pigeon names out of given array
+  data.each do |attributes, value|
+    value.each do |stats, names|
+      names.each do |name|
+        if final_pigeon_list[name] == nil
+          final_pigeon_list[name] = {}
+        end
+        if final_pigeon_list[name][attributes] == nil
+          final_pigeon_list[name][attributes] = []
+        end
+        final_pigeon_list[name][attributes].push(stats.to_s)
+      end
+    end
+  end
+final_pigeon_list
 end
